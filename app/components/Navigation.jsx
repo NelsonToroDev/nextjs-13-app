f6dcea940a574e4607bf0bcd6a022cbba73f7259
Nekero8 <nekero8@gmail.com>
2022-12-28 17:10:20 -0400

    Adding main layout and Navigation Component with custom styles using css module

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,29 @@
+import styles from './Navigation.module.css'
+import Link from 'next/link'
+
+const links = [
+  {
+    label: 'Home',
+    route: '/'
+  },
+  {
+    label: 'About',
+    route: '/about'
+  }
+]
+
+export function Navigation () {
+  return (
+    <header className={styles.header}>
+      <nav>
+        <ul className={styles.navigation}>
+          {links.map(({ label, route }) => (
+            <li key={route}>
+              <Link href={route}>{label}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  )
+}
--- app/layout.js
+++ app/layout.js
@@ -1,33 +1,14 @@
-import Link from 'next/link'
-const links = [
-  {
-    label: 'Home',
-    route: '/'
-  },
-  {
-    label: 'About',
-    route: '/about'
-  }
-]
+import '../styles/globals.css'
+import { Navigation } from './components/Navigation'
 
-export default function RootLayout({ children }) {
+export default function RootLayout ({ children }) {
   return (
     <html>
       <head>
         <title>My first app using Next Js 13</title>
       </head>
       <body>
-        <header>
-          <nav>
-            <ul>
-              {links.map(({ label, route }) => (
-                <li key={route}>
-                  <Link href={route}>{label}</Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </header>
+        <Navigation />
         {children}
       </body>
     </html>
